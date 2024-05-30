47b6e7b00638160abfd13272c24d9f674d04fcff
aiperi12 <aruslanbekova@gmail.com>
2024-05-29 19:31:06 -0500

    updated

--- session-6/main.tf
+++ session-6/main.tf
@@ -1,13 +1,13 @@
 module "ec2" {
 
-    source = "github.com/Aiperi12/terraform-session//modules/ec2"   
+    source = "github.com/Aiperi12/terraform-session/modules/ec2"   
     ami = "ami-01cd4de4363ab6ee8"
     instance_type = "t2.micro" 
     sg = [module.sg.security_group_id]
 }
 
 module "sg" {
-    source = "github.com/Aiperi12/terraform-session//modules/sg"
+    source = "github.com/Aiperi12/terraform-session/modules/sg"
     env = "dev"
     ports = ["22", "80", "443" ] 
   
